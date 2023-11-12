c8da9901cf014561a17aedc429abaf9c83f5d6b1
Nathanage3 <sisay954@gmail.com>
2023-11-12 18:58:40 +0300

    update for unittests

--- models/base_model.py
+++ models/base_model.py
@@ -55,56 +55,42 @@ class BaseModel:
         return dictionary
 '''
 import models
-import uuid
+from uuid import uuid4
 from datetime import datetime
 
 
-class BaseModel():
-    """A base class for all hbnb models.
-    """
+class BaseModel:
+    """Type class of BaseModel"""
 
     def __init__(self, *args, **kwargs):
-        """
-        instatiates an object with it's
-        attributes.
-        """
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
+        """Type method initialize"""
+        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for key, val in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
-            return
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        models.storage.new(self)
-
-    def __str__(self):
-        """
-        Returns the string representation
-        of the instances.
-        """
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
+                    setattr(self, key, datetime.strptime(val, timeformat))
+                elif key != '__class__':
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            models.storage.new(self)
 
     def save(self):
-        """
-        updated_at with the current datetime.
-        """
-        self.updated_at = datetime.now()
+        """Type method save"""
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """
-        returns a dictionary containing all keys/values
-        """
-        dict = {**self.__dict__}
-        dict['__class__'] = type(self).__name__
-        dict['created_at'] = dict['created_at'].isoformat()
-        dict['updated_at'] = dict['updated_at'].isoformat()
+        """Type method to_dict"""
+        rt_dict = self.__dict__.copy()
+        rt_dict["created_at"] = self.created_at.isoformat()
+        rt_dict["updated_at"] = self.updated_at.isoformat()
+        rt_dict["__class__"] = self.__class__.__name__
+        return rt_dict
 
-        return dict
+    def __str__(self):
+        """Type method __str__"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
